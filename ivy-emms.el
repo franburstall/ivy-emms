6f547f9f3dcbf0d5b88595760b3505c7195dc96b
Fran Burstall <fran.burstall@gmail.com>
2020-06-29 09:01:24 +0100

    * ivy-emms.el: document installation from MELPA

--- ivy-emms.el
+++ ivy-emms.el
@@ -26,7 +26,12 @@
 ;; This package offers an ivy interface to play, add and insert tracks
 ;; into the current EMMS playlist.
 
-;; Put this file somewhere in your load-path and load it with:
+;; Installation: install from MELPA with
+;;     (package-install 'ivy-emms)
+;; or
+;;     (use-package ivy-emms
+;;       :ensure t)
+;; or simply put this file somewhere in your load-path and load it with:
 ;;     (require 'ivy-emms)
 
 ;; Fire it up with
